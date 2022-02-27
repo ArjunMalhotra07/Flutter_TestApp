59cdadadf8827f70c6605ea2280cfba821b04cb3
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-02-27 15:14:55 +0530

    Revert "Functions, Classes, Routes"
    This reverts commit 91e18e3c9aaac6d5e18d4b3a548815f7209723b6.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
-import 'package:flutter_catalog/pages/loginPage.dart';
 
 void main() => runApp(MaterialApp(
       home: MyApp(),
@@ -9,22 +8,8 @@ void main() => runApp(MaterialApp(
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    bringVegies(thaila: true, rupees: 500);
-    return MaterialApp(
-      // home: HomePage(),
-      themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-      initialRoute: "/home",
-      routes: {
-        "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
-      },
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
-
-  bringVegies({@required thaila, int rupees = 100}) {}
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  int days = 30;
-  num temp = 30.5;
-  num tempNew = 45;
-  var day = "Tuesday"; //var day = 5  //var day = true
-  static const pi = 3.14; //cant be changed
-  String s = "Arjun";
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    num temp = 30.5;
+    num tempNew = 45;
+    var day = "Tuesday"; //var day = 5  //var day = true
+    const pi = 3.14; //cant be changed
+    String s = "Arjun";
     return Scaffold(
       appBar: AppBar(
-        //backgroundColor: Colors.grey[800],
+        backgroundColor: Colors.grey[800],
         title: const Text(
+          //'GirlScript Summer of Code $days $s',
           "Hello Mummy I Love You",
           style: TextStyle(color: Colors.white),
         ),
